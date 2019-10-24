7327044f3d5058a5ef3f273858583dba16a2a93d
Rohit Gupta <49038290+Rohit2021@users.noreply.github.com>
2019-10-24 16:36:43 -0400

    Add files via upload

--- Blue Bird Client.py
+++ Blue Bird Client.py
@@ -14,31 +14,135 @@ port = 9000
 s.connect((host,port))
 
 def header():
-    print("""             ██████╗ ██╗     ██╗   ██╗███████╗    ██████╗ ██╗██████╗ ██████╗ 
-             ██╔══██╗██║     ██║   ██║██╔════╝    ██╔══██╗██║██╔══██╗██╔══██╗ 
-             ██████╔╝██║     ██║   ██║█████╗      ██████╔╝██║██████╔╝██║  ██║
-             ██╔══██╗██║     ██║   ██║██╔══╝      ██╔══██╗██║██╔══██╗██║  ██║
-             ██████╔╝███████╗╚██████╔╝███████╗    ██████╔╝██║██║  ██║██████╔╝
-             ╚═════╝ ╚══════╝ ╚═════╝ ╚══════╝    ╚═════╝ ╚═╝╚═╝  ╚═╝╚═════╝ """)
+    print("""              ██████╗ ██╗     ██╗   ██╗███████╗    ██████╗ ██╗██████╗ ██████╗ 
+              ██╔══██╗██║     ██║   ██║██╔════╝    ██╔══██╗██║██╔══██╗██╔══██╗ 
+              ██████╔╝██║     ██║   ██║█████╗      ██████╔╝██║██████╔╝██║  ██║
+              ██╔══██╗██║     ██║   ██║██╔══╝      ██╔══██╗██║██╔══██╗██║  ██║
+              ██████╔╝███████╗╚██████╔╝███████╗    ██████╔╝██║██║  ██║██████╔╝
+              ╚═════╝ ╚══════╝ ╚═════╝ ╚══════╝    ╚═════╝ ╚═╝╚═╝  ╚═╝╚═════╝ """)
 
 def create_account():    #informs server that there is a need to create a new account
     s.send(login.encode())
-    print("Account Creation Menu\n")
+    cls2()
     set_name = input(str("\nSet Username: "))
     set_name = set_name.encode()
     s.send(set_name)
 
+    cls2()
     set_password = input(str("\nSet Password: "))
     set_password = set_password.encode()
     s.send(set_password)
-    
-    set_email = input(str("\nSet Email: "))
-    set_email = set_email.encode()
-    s.send(set_email)
 
-    set_zip = input(str("\nSet Zip Code : "))
-    set_zip = set_zip.encode()
-    s.send(set_zip)
+    cls2()
+    set_firstname = input(str("\nWhat's your first name?: "))
+    set_firstname = set_firstname.encode()
+    s.send(set_firstname)
+
+    cls2()
+    set_lastname = input(str("\nWhat's your last name?: "))
+    set_lastname = set_lastname.encode()
+    s.send(set_lastname)
+
+    cls2()
+    set_gender = input(str("\nWhat's your gender? Male('1') or Female('2') : "))
+    set_gender = set_gender.encode()
+    s.send(set_gender)
+
+    cls2()
+    set_interest = input(str("\nAre you interested in Men('1') or Women('2')? : "))
+    set_interest = set_interest.encode()
+    s.send(set_interest)
+
+    cls2()
+    set_age = input(str("\nWhat's your age? Examples: '23' or '35' : "))
+    set_age = set_age.encode()
+    s.send(set_age)
+
+    cls2()
+    set_child = input(str("\nDo you have children? No('1') or Yes('2') : "))
+    set_child = set_child.encode()
+    s.send(set_child)
+
+    cls2()
+    set_ethnic = input(str("\nWhat's your ethnic background? Examples: Caucasian, African-American, Asian, etc : "))
+    set_ethnic = set_ethnic.encode()
+    s.send(set_ethnic)
+
+    cls2()
+    set_religion = input(str("\nWhat's your religion? : "))
+    set_religion = set_religion.encode()
+    s.send(set_religion)
+
+    cls2()
+    set_school = input(str("\nWhat's your highest level of education? : "))
+    set_school = set_school.encode()
+    s.send(set_school)
+
+    cls2()
+    set_career = input(str("\nWhat's your occupation? : "))
+    set_career = set_career.encode()
+    s.send(set_career)
+
+    cls2()
+    set_sports = input(str("\nDo you like sports? No('1') or Yes('2') : "))
+    set_sports = set_sports.encode()
+    s.send(set_sports)
+
+    cls2()
+    set_football = input(str("\nDo you like football? No('1') or Yes('2') : "))
+    set_football = set_football.encode()
+    s.send(set_football)
+
+    cls2()
+    set_baseball = input(str("\nDo you like baseball? No('1') or Yes('2') : "))
+    set_baseball = set_baseball.encode()
+    s.send(set_baseball)
+
+    cls2()
+    set_basketball = input(str("\nDo you like basketball? No('1') or Yes('2') : "))
+    set_basketball = set_basketball.encode()
+    s.send(set_basketball)
+    
+    cls2()
+    set_soccer = input(str("\nDo you like soccer? No('1') or Yes('2') : "))
+    set_soccer = set_soccer.encode()
+    s.send(set_soccer)
+
+    cls2()
+    set_other = input(str("\nDo you like any other sport? No('1') or Yes('2') : "))
+    set_other = set_other.encode()
+    s.send(set_other)
+
+    cls2()
+    set_movies = input(str("\nDo you like movies? No('1') or Yes('2') : "))
+    set_movies = set_movies.encode()
+    s.send(set_movies)
+
+    cls2()
+    set_music = input(str("\nDo you like music? No('1') or Yes('2') : "))
+    set_music = set_music.encode()
+    s.send(set_music)
+
+    cls2()
+    set_dance = input(str("\nDo you like dance? No('1') or Yes('2') : "))
+    set_dance = set_dance.encode()
+    s.send(set_dance)
+
+    cls2()
+    set_social = input(str("\nDo you like social media? No('1') or Yes('2') : "))
+    set_social = set_social.encode()
+    s.send(set_social)
+
+    cls2()
+    set_reading = input(str("\nDo you like reading? No('1') or Yes('2') : "))
+    set_reading = set_reading.encode()
+    s.send(set_reading)
+
+    cls2()
+    set_bio = input(str("\nPlease complete the form by writing a few sentences,\nwhich will be you profile's bio? : "))
+    set_bio = set_bio.encode()
+    s.send(set_bio)
+
     print("\nAccount Created\n")
 
 def do_not_create_account():    #informs server that there is no need to create a new account
@@ -75,8 +179,13 @@ def cls():
     header()
     print("\n   MY PROFILE('1')      MESSAGES('2')      MATCH ME('3')      SEARCH('4')      OPTIONS('5)\n");
 
+def cls2():  #only for the create account function
+    os.system('cls' if os.name=='nt' else 'clear')
+    header()
+    print("\n                                  Account Creation Menu\n")
+
 header()
-login = input(str("\nWelcome - Create a new account(Enter '1'): "))
+login = input(str("\n\n                      Create a new account('1') or Login('2'): "))
 if (login == "1"):
      create_account()
 else:
--- Blue Bird Server.py
+++ Blue Bird Server.py
@@ -48,11 +48,71 @@ def create_new_account(conn):
     incoming_password = conn.recv(1024) 
     incoming_password = incoming_password.decode()
 
-    incoming_email = conn.recv(1024) 
-    incoming_email = incoming_email.decode()
+    incoming_fname = conn.recv(1024) 
+    incoming_fname = incoming_fname.decode()
 
-    incoming_zip = conn.recv(1024) 
-    incoming_zip = incoming_zip.decode()
+    incoming_lname = conn.recv(1024) 
+    incoming_lname = incoming_lname.decode()
+
+    incoming_gender = conn.recv(1024) 
+    incoming_gender = incoming_gender.decode()
+
+    incoming_interest = conn.recv(1024) 
+    incoming_interest = incoming_interest.decode()
+
+    incoming_age = conn.recv(1024) 
+    incoming_age = incoming_age.decode()
+
+    incoming_child = conn.recv(1024) 
+    incoming_child = incoming_child.decode()
+
+    incoming_ethnic = conn.recv(1024) 
+    incoming_ethnic = incoming_ethnic.decode()
+
+    incoming_religion = conn.recv(1024) 
+    incoming_religion = incoming_religion.decode()
+
+    incoming_school = conn.recv(1024) 
+    incoming_school = incoming_school.decode()
+
+    incoming_career = conn.recv(1024) 
+    incoming_career = incoming_career.decode()
+
+    incoming_sports = conn.recv(1024) 
+    incoming_sports = incoming_sports.decode() 
+
+    incoming_football = conn.recv(1024) 
+    incoming_football = incoming_football.decode()
+
+    incoming_baseball = conn.recv(1024) 
+    incoming_baseball = incoming_baseball.decode()
+
+    incoming_basketball = conn.recv(1024) 
+    incoming_basketball = incoming_basketball.decode() 
+
+    incoming_soccer = conn.recv(1024) 
+    incoming_soccer = incoming_soccer.decode()
+
+    incoming_other = conn.recv(1024) 
+    incoming_other = incoming_other.decode()
+
+    incoming_movies = conn.recv(1024) 
+    incoming_movies = incoming_movies.decode() 
+
+    incoming_music = conn.recv(1024) 
+    incoming_music = incoming_music.decode()
+
+    incoming_dance = conn.recv(1024) 
+    incoming_dance = incoming_dance.decode()
+
+    incoming_social = conn.recv(1024) 
+    incoming_social = incoming_social.decode()
+
+    incoming_reading = conn.recv(1024) 
+    incoming_reading = incoming_reading.decode()
+
+    incoming_bio = conn.recv(1024) 
+    incoming_bio = incoming_bio.decode()
 
     xfile = openpyxl.load_workbook('Users.xlsx')
     sheet = xfile.get_sheet_by_name('Sheet1')
@@ -63,8 +123,28 @@ def create_new_account(conn):
             sheet.cell(row=i + 1, column=1, value=1)
             sheet.cell(row=i + 1, column=2, value=incoming_username)
             sheet.cell(row=i + 1, column=3, value=incoming_password)
-            sheet.cell(row=i + 1, column=4, value=incoming_email)
-            sheet.cell(row=i + 1, column=5, value=incoming_zip)
+            sheet.cell(row=i + 1, column=4, value=incoming_fname)
+            sheet.cell(row=i + 1, column=5, value=incoming_lname)
+            sheet.cell(row=i + 1, column=6, value=incoming_gender)
+            sheet.cell(row=i + 1, column=7, value=incoming_interest)
+            sheet.cell(row=i + 1, column=8, value=incoming_age)
+            sheet.cell(row=i + 1, column=9, value=incoming_child)
+            sheet.cell(row=i + 1, column=10, value=incoming_ethnic)
+            sheet.cell(row=i + 1, column=11, value=incoming_religion)
+            sheet.cell(row=i + 1, column=12, value=incoming_school)
+            sheet.cell(row=i + 1, column=13, value=incoming_career)
+            sheet.cell(row=i + 1, column=14, value=incoming_sports)
+            sheet.cell(row=i + 1, column=15, value=incoming_football)
+            sheet.cell(row=i + 1, column=16, value=incoming_baseball)
+            sheet.cell(row=i + 1, column=17, value=incoming_basketball)
+            sheet.cell(row=i + 1, column=18, value=incoming_soccer)
+            sheet.cell(row=i + 1, column=19, value=incoming_other)
+            sheet.cell(row=i + 1, column=20, value=incoming_movies)
+            sheet.cell(row=i + 1, column=21, value=incoming_music)
+            sheet.cell(row=i + 1, column=22, value=incoming_dance)
+            sheet.cell(row=i + 1, column=23, value=incoming_social)
+            sheet.cell(row=i + 1, column=24, value=incoming_reading)
+            sheet.cell(row=i + 1, column=25, value=incoming_bio)
             break
 
     xfile.save('Users.xlsx')
@@ -124,10 +204,23 @@ def message(conn):         #expands on the message command by asking the user wh
     conn.send(response.encode('utf-8'))
     console(conn)   #returns user to console function
 
+def view_profile():
+    print("Placeholder\n")
+
 def console(conn):      #Allows clients to enter in one of two commands, list and message
     while True:
         incoming_message = conn.recv(1024) 
         incoming_message = incoming_message.decode()
+
+        if incoming_message == "1":
+            view_profile()
+        #if incoming_message == "2":
+
+        #if incoming_message == "3":
+        
+        #if incoming_message == "4":
+
+        #if incoming_message == "5":
         
         if incoming_message == "list":
             print("\nListing connections ...\n",Names)
